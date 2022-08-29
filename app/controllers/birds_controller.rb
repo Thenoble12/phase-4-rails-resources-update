c94a30b56ac14ab5cbc44e7085b4ade070350f92
Elijah Noble <elijahnobledev@gmail.com>
2022-08-28 19:52:30 -0700

    Assignment Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,18 +1,27 @@
 class BirdsController < ApplicationController
 
-  # GET /birds
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+  end
+
+  def update
+    bird = Bird.find_by(id: params[:id])
+    bird.update(bird_params)
+    render json: bird
+  end
+
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
   def create
     bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
@@ -25,7 +34,7 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
